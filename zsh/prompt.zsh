242b35554e06e403745a759993bbba62d99afa83
gfestari <gaston.festari@mercadolibre.com>
2013-12-04 11:06:22 -0200

    Update config modules refs Set prompt to adam1 defaults

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,7 @@
 autoload -Uz promptinit && promptinit # use the included prompt functionality
 
 # pimp my prompt
-prompt adam1 magenta blue green
+prompt adam1
 
 # right-hand prompt with exit status error and time
 RPS1=%(?..[ %B%F{yellow}%?%b%f ])
