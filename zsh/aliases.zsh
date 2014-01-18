953f7726d49805c0efa9d9b801be7cfbeba7d666
gfestari <cilindrox@gmail.com>
2014-01-18 12:05:58 -0200

    Mindhack for using most as pager

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -42,6 +42,8 @@ alias ags='ag --stats'
 alias gits='git status -sb'
 alias gemupd='gem update --system && gem update'
 
+alias more=most
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
