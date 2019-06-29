0365feed4d41a133207822afd46ecc604d9a904d
Gaston Festari <cilindrox@gmail.com>
2019-06-29 09:39:01 -0300

    feat: update install links

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ brew bundle
 
 echo 'Linking settings...'
 
-for f in 'tmux.conf' 'aliases' 'editorconfig' 'zshenv' 'zshrc' 'gitconfig' 'gemrc' 'gitignore' 'agignore' 'npmrc' 'zlogout' 'ssh' 'gnupg' 'dircolors'; do
+for f in 'tmux.conf' 'aliases' 'editorconfig' 'zshenv' 'zshrc' 'gitconfig' 'gemrc' 'gitignore' 'gitmessage' 'agignore' 'npmrc' 'zlogout' 'ssh' 'gnupg' 'dircolors'; do
   ln -sf "$PWD/${f}" "${HOME}/.${f}"
 done
 
